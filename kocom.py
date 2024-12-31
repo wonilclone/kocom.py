d4009e9c9e37fd50be0ce14135b118cac0d1307f
wonil5542 <69750098+wonilclone@users.noreply.github.com>
2024-12-31 11:13:19 +0900

    도커 파일 수정

--- kocom.py
+++ kocom.py
@@ -9,6 +9,7 @@
  apt-get install mosquitto
  python3 -m pip install pyserial
  python3 -m pip install paho-mqtt
+ python3 -m pip install typing_extensions
 '''
 import os
 import time
@@ -51,7 +52,7 @@ room_h_dic = {'livingroom':'00', 'myhome':'00', 'room1':'01', 'room2':'02', 'roo
 # mqtt functions ----------------------------
 
 def init_mqttc():
-    mqttc = mqtt.Client()
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
     mqttc.on_message = mqtt_on_message
     mqttc.on_subscribe = mqtt_on_subscribe
     mqttc.on_connect = mqtt_on_connect
